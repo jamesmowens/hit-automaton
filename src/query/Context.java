6275ae1fc732ef2f85a9bc6c784ff1f42f6c99ed
Nicholas Fajardo <Nicholas Fajardo@192.168.13.135>
2017-01-02 20:36:01 -0700

    Updating the interfaces used in the Query package

--- src/query/Context.java
+++ src/query/Context.java
@@ -2,4 +2,7 @@ package query;
 
 public interface Context {
 
+	//whatever the Context might be, it must have an evaluating factor that decides if it was fulfilled or not.
+	//This should return a boolean or not depending on the result of the evaluation
+	public boolean evaluate();
 }
--- src/query/Query.java
+++ src/query/Query.java
@@ -1,5 +1,22 @@
 package query;
 
-public interface Query {
+import java.util.HashSet;
 
+//This is the interface I will use to define some of the method headers.
+public class Query {
+	
+	private HashSet<Event> events;
+	
+	public Query(){}
+	
+	/**
+	 * This method just adds an event to the event list in this class
+	 * @param event
+	 */
+	public void addEvent(Context context)
+	{
+		events.add(new Event(context));
+	}
+	
+	//public void printData
 }
