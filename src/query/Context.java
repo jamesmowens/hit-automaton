d1ab20241f42458339b8f90d39438dd75aa86301
Nicholas Fajardo <Nicholas Fajardo@PhantomYoga>
2017-01-03 11:03:30 -0700

    Further changes with Query package
    - Deleted Event class (redundant class)
    - Made the query class contain an event and stats about the event

--- src/query/Context.java
+++ src/query/Context.java
@@ -1,8 +1,14 @@
 package query;
 
-public interface Context {
+//I am adding this so we can compare the context at hand.
+public interface Context extends Comparable<Context> {
 
-	//whatever the Context might be, it must have an evaluating factor that decides if it was fulfilled or not.
-	//This should return a boolean or not depending on the result of the evaluation
-	public boolean evaluate();
+	/**
+	 * Whatever the Context might be, it must have an evaluating factor that decides if it was fulfilled or not
+	 * This should return a boolean or not depending on the result of the evaluation
+	 * The defined context should use helper classes to access and fully evaluate the context at hand.
+	 * @param parameter (integer). We can use this as a price, a binary true or false, or anything else 
+	 * @return the result of the evaluation.
+	 */
+	public boolean evaluate(int parameter);
 }
--- src/query/Query.java
+++ src/query/Query.java
@@ -2,21 +2,68 @@ package query;
 
 import java.util.HashSet;
 
-//This is the interface I will use to define some of the method headers.
+/**
+ * This Query is the class that is used for query definitions
+ * It includes the use of the Context class in the query package
+ * This calss also contains statistics for a given context, such as the number of times the context was evaluated, or the success rates of the context
+ * @author Nicholas Fajardo
+ */
 public class Query {
 	
-	private HashSet<Event> events;
-	
-	public Query(){}
-	
 	/**
-	 * This method just adds an event to the event list in this class
-	 * @param event
+	 * These private instance variables are used to keep track of statistics and the context to evaluate.
 	 */
-	public void addEvent(Context context)
+	private Context context;
+	private int numberOfEvaluated;
+	private int sucsesses;
+
+	/**
+	 * This class is basically a context with data.
+	 * @param context
+	 */
+	public Query(Context context)
 	{
-		events.add(new Event(context));
+		this.context = context;
+		this.numberOfEvaluated = 0;
+		this.sucsesses = 0;
 	}
 	
-	//public void printData
+	/**
+	 * Returns the context of the query
+	 * @return The context of the query
+	 */
+	public Context fetchContext()
+	{
+		return this.context;
+	}
+	
+	/**
+	 * Returns the successes of the query so far
+	 * @return Returns the successes of the query so far
+	 */
+	public int fetchPasses(){
+		return this.sucsesses;
+	}
+	
+	/**
+	 * Returns the failures of the query so far
+	 * @return Returns the failures of the query so far
+	 */
+	public int fetchFails(){
+		return this.numberOfEvaluated - this.sucsesses;
+	}
+	
+	/**
+	 * This runs an evaluation of the query, in which the user is expected to update the query variable to pass in beforehand
+	 * This should also update the statistics inside the class
+	 * @param parameter (integer), this is for the context, documentation can be found there for the specifics.
+	 * @return the result of the context evaluation
+	 */
+	public boolean evaluateContext(int param){
+		boolean rv = context.evaluate(param);
+		if(rv)
+			sucsesses++;
+		numberOfEvaluated++;
+		return rv;
+	}
 }
