44082739ae456182071d581d6c961e762e15f873
Tominator368 <thomas.schweich@gmail.com>
2017-01-07 19:00:52 -0600

    Added documentation for layout/SettingsAccessor.java. Also pushing my workspace.xml file in hopes of it making the GSON library work for others.

--- src/edu/usfca/vas/layout/SettingsAccessor.java
+++ src/edu/usfca/vas/layout/SettingsAccessor.java
@@ -11,25 +11,46 @@ import java.util.Map;
 import java.util.Scanner;
 
 /**
- * Created by thoma on 1/6/2017.
+ * Created by Thomas on 1/6/2017.
+ *
+ * Singleton for accessing settings stored in {String : String, ...} format
  */
 public enum SettingsAccessor {
     INSTANCE;
 
     private Map<String, String> colors;
 
+    /**
+     * Creates the instance (INSTANCE) of SettingsAccessor, reading json file(s)
+     */
     SettingsAccessor() {
-        Gson gson = new Gson();
-        try {
-            String json = new Scanner(new File("src/edu/usfca/vas/layout/colors.json")).useDelimiter("\\Z").next();
-            colors = gson.fromJson(json, new TypeToken<Map<String, String>>(){}.getType());
-        } catch (FileNotFoundException e) {
-            System.err.println("Couldn't find colors.json" + " cwd: " + new File(".").getAbsoluteFile());
-            colors = new HashMap<String, String>();
-            e.printStackTrace();
-        }
+        colors = accessSettings("src/edu/usfca/vas/layout/colors.json");
     }
 
+    /**
+     * Returns the settings stored at the given path in {@code {String : String, ...}} format in a {@code Map<String, String>}
+     * @param path The location of the settings file
+     * @return {@code Map<String, String>} of the (key, value) pairs in the settings file
+     */
+    private static Map<String, String> accessSettings(String path) {
+        Gson gson = new Gson();
+        Map<String, String> result;
+        try {
+            String json = new Scanner(new File(path)).useDelimiter("\\Z").next();
+            result = gson.fromJson(json, new TypeToken<Map<String, String>>(){}.getType());
+        } catch (FileNotFoundException e) {
+            System.err.println("Couldn't find colors.json" + " cwd: " + new File(".").getAbsoluteFile());
+            result = new HashMap<String, String>();
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Retrieves the color value stored under {@param key} from colors.json
+     * @param key The key for the value to retrieve in colors.json
+     * @return Color representing the hex value from colors.json
+     */
     public static Color getColor(String key) {
         try {
             return Color.decode(INSTANCE.colors.get(key));
