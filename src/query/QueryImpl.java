169d55cf142395aea6fe0cfac74cb829ae41f177
MaryAnn VanValkenburg <mevanvalkenburg@wpi.edu>
2017-01-22 17:40:48 -0500

    Update QueryImpl
    Added some comments

--- src/query/QueryImpl.java
+++ src/query/QueryImpl.java
@@ -2,7 +2,7 @@ package query;
 
 public class QueryImpl implements Query{
 	
-	private String frequency;
+	private String frequency = new String();
 	private String condition = new String();
 	private String set = new String();
 	
@@ -30,18 +30,30 @@ public class QueryImpl implements Query{
 		return this.set;
 	}
 	
+	/**
+	 * Runs the query condition and returns the result
+	 * @return boolean signifying the result, and also represents if the result method is run
+	 */
 	@Override
 	public boolean evaluate() {
-		// TODO Auto-generated method stub
+		// TODO Parse the String "condition" into an expression and evaluate
 		return false;
 	}
 
+	/**
+	 * Runs the change that the query makes if the condition is met
+	 * @return boolean depending if it successfully ran or not
+	 */
 	@Override
 	public boolean result() {
-		// TODO Auto-generated method stub
+		// TODO run evaluate(), if TRUE, apply "set"
 		return false;
 	}
 
+	/**
+	 * Access to a descriptive string of the query
+	 * @return the description of the query
+	 */
 	@Override
 	public String queryInfo() {
 		return new String("Frequency: "+frequency+", Condition: "+condition+", Set: "+set);
