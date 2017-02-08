080d019dcfd7526c269d9b9c1f5816231a23e787
James Owens <jamesmarshallowens@gmail.com>
2017-02-08 13:54:31 -0500

    Chaged transition query to match expression implemetation

--- src/Query/TransitionQuery.java
+++ src/Query/TransitionQuery.java
@@ -4,7 +4,8 @@ import connection.Step;
 
 public class TransitionQuery extends Query {
 	
-	Step myStep;  
+	Step successStep;  
+	Step currentStep; 
 
 	/**
 	 * 
@@ -16,7 +17,7 @@ public class TransitionQuery extends Query {
 	public TransitionQuery(Condition expression, String pertainingState, String queryName, Step successStep) {
 		super(expression, pertainingState, queryName);
 		// TODO Auto-generated constructor 
-		myStep = successStep;
+		this.successStep = successStep;
 		
 
 	}
@@ -24,17 +25,27 @@ public class TransitionQuery extends Query {
 	@Override
 	public void run(int parameter) {
 		// TODO Auto-generated method stub
+		String expression = ex.getExpression();
+		// An expression has an existing variable, a comparison operator, and a parameter to compare
+		// TODO parameter to compare is currently stored as "?" in expression string
+		String evaluable = expression.replace("?", ""+parameter);
+		// search expression, input parameter, evaluate new expression
+		boolean result = this.ex.evaluate(evaluable);
+		if (result) {
+			currentStep =  successStep;
+		} else {
+			currentStep = new Step(successStep.getSource(),successStep.getSource(),"Didn't Move");;
+		}
 		
 		
 	}
 	
-	public Step getResultingStep() {
-		if(expression.evaluate()){
-			return myStep;
-		} else {
-			return new Step(myStep.getSource(),myStep.getSource(),"Didn't Move"); 
-		}
-		
+	
+	/*
+	 * Returns the step set by the last run of the Query
+	 */
+	public Step getStep() {
+		return currentStep; 
 	}
 
 }
