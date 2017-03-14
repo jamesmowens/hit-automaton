6dae863095d350574a77140ca4d4abd84faf21fa
James Owens <jamesmarshallowens@gmail.com>
2017-03-14 12:04:40 -0400

    Removed Chaos (again)

--- CHAOS/com/hp/hpl/CHAOS/HIT/EventID.java
+++
@@ -1,8 +0,0 @@
-package com.hp.hpl.CHAOS.HIT;
-
-public class EventID extends Identifier {
-	
-	public EventID(String n) {
-		super(n);
-	}
-}
--- CHAOS/com/hp/hpl/CHAOS/HIT/StateID.java
+++
@@ -1,8 +0,0 @@
-package com.hp.hpl.CHAOS.HIT;
-
-public class StateID extends Identifier {
-	
-	public StateID(String n) {
-		super(n);
-	}
-}
